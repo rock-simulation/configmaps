8bbf9dcb5a4918e2d54b7d938505cb7aef60aab2
Martin Zenzes <martin.zenzes@dfki.de>
2016-03-03 15:36:46 +0100

    add compatibility headers after renaming
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

---
+++ src/ConfigData.h
@@ -0,0 +1,5 @@
+// MZ: renamed "src/ConfigData.h" to "src/ConfigData.hpp" at 03.03.2016
+//
+// keep this file around for a month or so, then delete it
+#warning "do not include this file, use 'ConfigData.hpp' instead"
+#include "ConfigData.hpp"
---
+++ src/FIFOMap.h
@@ -0,0 +1,5 @@
+// MZ: renamed "src/FIFOMap.h" to "src/FIFOMap.hpp" at 03.03.2016
+//
+// keep this file around for a month or so, then delete it
+#warning "do not include this file, use 'FIFOMap.hpp' instead"
+#include "FIFOMap.hpp"
