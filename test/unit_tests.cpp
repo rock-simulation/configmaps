030a9b39112bb8a6fc6bc38b25dee70c26aa282f
Julian Liersch <julian.liersch@dfki.de>
2024-09-02 10:22:18 +0200

    added config schema unittest for min and max values

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -10,15 +10,15 @@ TEST_CASE("ConfigMap", "boolean")
 {
 
     ConfigMap map;
-    map["abstract_positive"] = "true"; 
-    map["abstract_negative"] = "false"; 
+    map["abstract_positive"] = "true";
+    map["abstract_negative"] = "false";
     REQUIRE((bool)map["abstract_positive"] == true);
     REQUIRE((bool)map["abstract_negative"] == false);
 
-    map["abstract_literal"] = ConfigAtom(true); 
+    map["abstract_literal"] = ConfigAtom(true);
     REQUIRE((bool)map["abstract_literal"] == true);
 
-    map["abstract_literal"] = ConfigAtom(false); 
+    map["abstract_literal"] = ConfigAtom(false);
     REQUIRE((bool)map["abstract_literal"] == false);
 
     ConfigMap recovered = ConfigMap::fromJsonString(map.toJsonString());
@@ -65,7 +65,7 @@ TEST_CASE("ConfigSchema", "validate")
     obj["objects"][1]["title"] = "test tes2t";
     obj["objects"][1]["description"] = "test description2";
     obj["objects"][1]["is_new"] = false;
-    
+
     REQUIRE(cs.validate(obj));
 }
 
@@ -101,9 +101,195 @@ TEST_CASE("ODEInertia_schema", "validate"){
     config["objects"][1]["title"] = "test tes2t";
     config["objects"][1]["description"] = "test description2";
     config["objects"][1]["is_new"] = false;
-    
+
     std::cout << "Config:\n" << config.toYamlString() << std::endl;
 
     REQUIRE(cs.validate(config));
 
 }
+
+TEST_CASE("min_max_tests", "checks valid and invalid values for integers and numbers regarding minium and maximum values")
+{
+    ConfigMap schema = ConfigMap::fromYamlFile("schema/min_max_test_schema.yaml");
+    ConfigSchema cs(schema);
+    std::cout << "Schema:\n" << schema.toYamlString() << std::endl << std::endl;
+
+
+    { // valids
+        ConfigMap config;
+        config["only_min_number_intmin"] = 15.5;
+        config["only_min_number_doublemin"] = 15.5;
+        config["only_min_int_intmin"] = 15;
+        config["only_min_int_doublemin"] = 16;
+        config["only_max_number_intmax"] = 1.0;
+        config["only_max_number_doublemax"] = 1.5;
+        config["only_max_int_intmax"] = 1;
+        config["only_max_int_doublemax"] = 1;
+        config["both_number_intvals"] = 7.0;
+        config["both_number_doublevals"] = 7.5;
+        config["both_int_intvals"] = 7;
+        config["both_int_doublevals"] = 7;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(cs.validate(config));
+    }
+
+    { // valid min boundaries
+        ConfigMap config;
+        config["only_min_number_intmin"] = 5.0;
+        config["only_min_number_doublemin"] = 5.5;
+        config["only_min_int_intmin"] = 5;
+        config["only_min_int_doublemin"] = 6;
+        config["both_number_intvals"] = 5.0;
+        config["both_number_doublevals"] = 5.5;
+        config["both_int_intvals"] = 5;
+        config["both_int_doublevals"] = 6;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(cs.validate(config));
+    }
+
+    { // valid max boundaries
+        ConfigMap config;
+        config["only_max_number_intmax"] = 5.0;
+        config["only_max_number_doublemax"] = 5.5;
+        config["only_max_int_intmax"] = 5;
+        config["only_max_int_doublemax"] = 5;
+        config["both_number_intvals"] = 10.0;
+        config["both_number_doublevals"] = 10.5;
+        config["both_int_intvals"] = 10;
+        config["both_int_doublevals"] = 10;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(cs.validate(config));
+    }
+
+    { // invalid min number int
+        ConfigMap config;
+        config["only_min_number_intmin"] = 4.9;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid min number double
+        ConfigMap config;
+        config["only_min_number_doublemin"] = 5.4;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid min int int
+        ConfigMap config;
+        config["only_min_int_intmin"] = 4;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid min int double
+        ConfigMap config;
+        config["only_min_int_doublemin"] = 5;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid max number int
+        ConfigMap config;
+        config["only_max_number_intmax"] = 5.1;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid max number double
+        ConfigMap config;
+        config["only_max_number_doublemax"] = 5.6;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid max int int
+        ConfigMap config;
+        config["only_max_int_intmax"] = 6;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid max int double
+        ConfigMap config;
+        config["only_max_int_doublemax"] = 6;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid both number int low
+        ConfigMap config;
+        config["both_number_intvals"] = 4.9;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid both number double low
+        ConfigMap config;
+        config["both_number_doublevals"] = 5.4;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid both int int low
+        ConfigMap config;
+        config["both_int_intvals"] = 4;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid both int double low
+        ConfigMap config;
+        config["both_int_doublevals"] = 5.4;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid both number int high
+        ConfigMap config;
+        config["both_number_intvals"] = 10.1;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid both number double high
+        ConfigMap config;
+        config["both_number_doublevals"] = 10.6;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid both int int high
+        ConfigMap config;
+        config["both_int_intvals"] = 11;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+
+    { // invalid both int double high
+        ConfigMap config;
+        config["both_int_doublevals"] = 11;
+        std::cout << "Config:\n" << config.toYamlString() << std::endl;
+
+        REQUIRE(!cs.validate(config));
+    }
+}
+
